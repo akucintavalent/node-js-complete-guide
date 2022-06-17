62a117e6d9106f5cccc21c38902198811b0b6f5a
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-17 22:42:37 +0200

    Remove unnecessary route

--- app.js
+++ app.js
@@ -7,11 +7,6 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', (req, res, next) => {
-  console.log('This always runs');
-  next();
-});
-
 app.use('/add-product', (req, res, next) => {
   console.log('In another middleware');
   res.send('<form action="product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
