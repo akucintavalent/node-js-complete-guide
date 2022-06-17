7cf84a1fb0285a0ae809a5afbe90bdedadc3ba05
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-17 22:53:23 +0200

    Move shop routes to /routes/shop.js

--- app.js
+++ app.js
@@ -6,15 +6,13 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
 
-app.use('/', (req, res, next) => {
-  console.log('In another middleware');
-  res.send('<h1>Hello from Express</h1>');
-})
+app.use(shopRoutes);
 
 app.listen(3000); // is equvalent to the line below
 // const server = http.createServer(app); server.listen(3000);
