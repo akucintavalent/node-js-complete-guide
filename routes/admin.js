545b18e4944b28b23e552e44c52e2f21e7b99275
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-17 22:58:36 +0200

    Refactor the code a bit

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/admin/add-product', (req, res, next) => {
+router.get('add-product', (req, res, next) => {
   res.send('<form action="add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 })
 
-router.post('/admin/add-product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
