9b3c199d466864372206212b8ac3c4b7c168f05b
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-17 20:49:43 +0200

    Use app listen

--- app.js
+++ app.js
@@ -14,6 +14,5 @@ app.use((req, res, next) => {
   res.send('<h1>Hello from Express</h1>');
 })
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000); // is equvalent to the line below
+// const server = http.createServer(app); server.listen(3000);
