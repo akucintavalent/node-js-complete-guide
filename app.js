bb3f2cb88d8ac32ec574dcf8537cf75c6932bb26
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-17 22:45:48 +0200

    Move two middlewares to admin.js

--- app.js
+++ app.js
@@ -7,16 +7,6 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware');
-  res.send('<form action="product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-})
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
 app.use('/', (req, res, next) => {
   console.log('In another middleware');
   res.send('<h1>Hello from Express</h1>');
