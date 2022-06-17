3461528e04e92dd928f5d355ba3f33bd469f033b
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-17 22:55:50 +0200

    Add 404 page

--- app.js
+++ app.js
@@ -14,5 +14,9 @@ app.use(adminRoutes);
 
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
+});
+
 app.listen(3000); // is equvalent to the line below
 // const server = http.createServer(app); server.listen(3000);
