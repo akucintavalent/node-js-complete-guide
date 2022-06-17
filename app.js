21b22edb9e465ab447c16256fd13de987dd3d871
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-17 20:18:01 +0200

    Use middleware that sends a response

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const app = express();
 
 app.use((req, res, next) => {
   console.log('In the middleware');
+  next();
 });
+
+app.use((req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>Hello from Express</h1>')
+})
 
 const server = http.createServer(app);
 
