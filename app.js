e9191cf18590598053b7fa61df233d5aaf5bf53e
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-17 22:31:28 +0200

    Add parser

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', (req, res, next) => {
   console.log('This always runs');
