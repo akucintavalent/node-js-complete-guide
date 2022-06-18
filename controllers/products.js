6d92e66a37165f3f641b9512cf6f9e123482c9d6
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-18 22:10:21 +0200

    Refactor product.js model

--- controllers/products.js
+++ controllers/products.js
@@ -5,19 +5,20 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: 'Add Product',
     path: '/admin/add-product'
   });
-}
+};
 
 exports.postAddProduct = (req, res, next) => {
   const product = new Product(req.body.title);
   product.save();
   res.redirect('/');
-}
+};
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
+  Product.fetchAll((products) => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+    });
   });
-}
+};
