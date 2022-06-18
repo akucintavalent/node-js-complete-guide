cd137988740d208029a30c8ea6972abf90147ced
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-18 19:58:06 +0200

    Create 404.ejs file

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs({ layoutDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs' }));
-app.set('view engine', 'hbs'); // We wanna compile dynamic html with pug template engine
+app.set('view engine', 'ejs'); // We wanna compile dynamic html with pug template engine
 app.set('views', 'views'); // We wanna use views folder for templates
 
 const adminData = require('./routes/admin');
@@ -22,7 +20,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { docTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000); // is equvalent to the line below
