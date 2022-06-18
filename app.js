8a3169445f0dbbc4fc89f7de137f51801a6a7656
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-18 13:10:51 +0200

    Use pug view engine

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
