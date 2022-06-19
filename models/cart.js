15fa91b143fbae0b06fa20c0c179b0199b785c6e
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-19 15:47:21 +0200

    Refactor adding to cart a bit

--- models/cart.js
+++ models/cart.js
@@ -5,7 +5,7 @@ const Product = require('./product');
 const p = path.join(path.dirname(process.mainModule.filename), 'data', 'cart.json');
 
 module.exports = class Cart {
-  static addProduct(id) {
+  static addProduct(id, productPrice) {
     // Fetch the previous cart
     fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
@@ -25,11 +25,9 @@ module.exports = class Cart {
         updatedProduct = { id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
       }
-      Product.findById(id, (product) => {
-        cart.totalPrice += +product.price;
-        fs.writeFile(p, JSON.stringify(cart), (err) => {
-          console.log(err);
-        });
+      cart.totalPrice += +productPrice;
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
       });
     });
   }
