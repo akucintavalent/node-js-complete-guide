c89f6d9be5b37c65900471a7ca4760ce3aea4ccf
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-19 13:24:43 +0200

    Add 'Add to Cart'button to product details page

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,9 +12,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  // console.log(prodId);
   Product.findById(prodId, (product) => {
-    // console.log('product=', product)
     if (product) {
       res.render('shop/product-detail', { 
         product: product,
