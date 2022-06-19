e5789983710617260b7f3292bfec192df63de9eb
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-19 16:05:07 +0200

    Start implementing edit product form

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,9 +1,9 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
-    path: '/admin/add-product'
+    path: '/admin/edit-product'
   });
 };
 
@@ -17,6 +17,25 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product
+    });
+  });
 };
 
 exports.getProducts = (req, res, next) => {
