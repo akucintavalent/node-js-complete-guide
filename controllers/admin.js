99acb8e27d5e9a7895312e8e26abafff128c2356
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-21 00:25:56 +0200

    Fix getEditProduct middleware in admin controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -30,8 +30,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then((product) => {
+  req.user.getProducts({ where: { id: prodId } })
+  // Product.findByPk(prodId)
+    .then(([product]) => {
       if (!product) {
         return res.redirect('/');
       }
