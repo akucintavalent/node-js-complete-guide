242bf7f65686bb5f277abb7dd97bdb4f520d5518
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-21 00:01:24 +0200

    Create relations in the database

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');;
 
 const app = express();
 
@@ -24,8 +26,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequelize
-  .sync()
+  .sync({ force: true }) // force: true will drop the table if it already exists
   .then(result => {
     // console.log(result);
     app.listen(3000);
