3da71c1593c9b6a57e3986c8e38df1b33446ed73
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-20 23:07:47 +0200

    Create tables related to models in app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -24,5 +24,15 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000); // is equvalent to the line below
+sequelize
+  .sync()
+  .then(result => {
+    console.log(result);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+// app.listen(3000); // is equvalent to the line below
 // const server = http.createServer(app); server.listen(3000);
