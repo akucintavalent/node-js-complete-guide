5f86128ea5369fc9ac395186b28ea835cb54464f
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-21 21:14:38 +0200

    Implement displaying orders in the orders view

--- controllers/shop.js
+++ controllers/shop.js
@@ -137,8 +137,14 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders',
-  });
+  req.user
+    .getOrders({ include: ['products'] })
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders,
+      });
+    })
+    .catch(err => console.log(err));
 };
